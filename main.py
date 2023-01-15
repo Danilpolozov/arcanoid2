b86a4343d92371fadf571380ae32737aff83f9fd
Danil <danil@123e.com>
2023-01-15 14:09:22 +0200

    obstacle

--- main.py
+++ main.py
@@ -19,6 +19,32 @@ class Paddle:
                                 self.color)
 
 
+class Ball:
+    def __init__(self):
+        self.x = WIDTH / 2
+        self.y = HEIGHT / 2
+        self.radius = 10
+        self.color = (255, 255, 255)
+        self.speed_x = random.randint(2, 5)
+        self.speed_y = random.randint(2, 5)
+
+    def update(self, dt):
+        self.x += self.speed_x
+        self.y += self.speed_y
+        if self.x + self.radius >= WIDTH or self.x - self.radius <= 0:
+            self.speed_x = -self.speed_x
+        if self.y + self.radius >= HEIGHT or self.y - self.radius <= 0:
+            if self.y + self.radius >= HEIGHT:
+                global lives
+                lives = lives - 1
+            self.speed_y = -self.speed_y
+
+    def draw(self):
+        screen.draw.circle((self.x, self.y), self.radius, self.color)
+
+
+class Obstacle:
+	pass
 
 
 pgzrun.go()
