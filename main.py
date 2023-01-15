611d8ba28449f0f75744bc6c6687ca40e3455731
Danil <danil@123e.com>
2023-01-15 14:08:48 +0200

    paddle implement

--- main.py
+++ main.py
@@ -7,7 +7,17 @@ HEIGHT = 800
 
 
 class Paddle:
-	pass
+    def __init__(self):
+        self.x = WIDTH / 2
+        self.y = HEIGHT - 20
+        self.width = 80
+        self.height = 20
+        self.color = (255, 255, 255)
+
+    def draw(self):
+        screen.draw.filled_rect(Rect((self.x - self.width / 2, self.y - self.height / 2), (self.width, self.height)),
+                                self.color)
+
 
 
 
